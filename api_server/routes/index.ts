514bc3041faada3347e0010e9ae3885d0d02ec8e
Hector Toral <hectortoraltrabajos@gmail.com>
2023-02-03 01:03:13 +0100

    Update index.ts

--- api_server/routes/index.ts
+++ api_server/routes/index.ts
@@ -5,7 +5,6 @@ import passport from 'passport'
 const router = express.Router()
 
 router.get('/ping', indexCtrl.pong)
-router.get('/ping-v2', indexCtrl.protectedPong)
 router.get('/ping-v2', passport.authenticate('jwt', { session: false }), indexCtrl.protectedPong)
 
 export default router
