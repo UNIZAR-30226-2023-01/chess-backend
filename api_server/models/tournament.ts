eef0845d9c9bd3e01a9f73b41640557ad8bfc72b
hector <hectortoraltrabajos@gmail.com>
2023-03-24 17:30:32 +0100

    documentacion fix

--- api_server/models/tournament.ts
+++ api_server/models/tournament.ts
@@ -1,7 +1,9 @@
 import { model, Schema, Document } from 'mongoose'
+import { UserModel } from '@models/user'
 
 export interface TournamentDocument extends Document {
   name: string
+  participants: [typeof UserModel]
 }
 
 const TournamentSchema = new Schema<TournamentDocument>({
@@ -9,7 +11,11 @@ const TournamentSchema = new Schema<TournamentDocument>({
     type: String,
     unique: true,
     required: true
-  }
+  },
+  participants: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }]
 }, {
   timestamps: true
 })
