ed9fd7d5f57d66ae944ad075f103e7420ed13090
hector <hectortoraltrabajos@gmail.com>
2023-04-01 19:33:49 +0200

    deploy.sh pendiente de modificar la parte de express

--- deploy.sh
+++ deploy.sh
@@ -21,35 +21,26 @@
 # It also requires that the user has access to the docker daemon and that the
 # user is in the docker group.
 
-EXPRESS_SERVER="chess-backend-express"
-PROXY_SERVER="chess-backend-nginx"
+EXPRESS_SERVER="chess-backend_express"
+PROXY_SERVER="chess-backend_nginx"
 SERVICE_NAME=$1
 
 if [ "$SERVICE_NAME" == "" ]; then
-  echo "Usage: ./deploy.sh [service]"
+  echo "Usage: ./deploy.sh [nginx | express]"
   exit 1
 fi
 
 if [ "$SERVICE_NAME" = "express" ]; then
-
+  git pull && npm install && npm run tsc
+  docker-compose down
+  docker rmi $EXPRESS_SERVER
+  docker-compose up -d $SERVICE_NAME
 elif [ "$SERVICE_NAME" = "nginx" ]; then
-  # Detener y eliminar el contenedor actual
-  docker stop chess-backend-express
-  docker rm chess-backend-express
-
-  # Limpiar el sistema de contenedores basura
-  docker system prune -f
-
-  # Eliminar la imagen actual
-  docker rmi chess-backend-express
-  
-  # Construir la imagen
-  docker-compose up -d nginx
-
+  git pull
+  docker-compose down
+  docker rmi $PROXY_SERVER
+  docker-compose up -d $SERVICE_NAME
 fi
 
 echo "Done"
 exit 0
-
-
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=chess-backend-nginx --format="{{.ID}}")) >/dev/null 2>&1 && docker rmi chess-backend-nginx >/dev/null 2>&1
