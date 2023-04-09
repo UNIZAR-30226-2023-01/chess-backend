6d09f50a19d524b8d5d373429beadfe9b2cd531c
Fernando <800989@unizar.es>
2023-04-10 00:55:56 +0200

    Script de compilacion mejorado

--- stockfish-compile.sh
+++ stockfish-compile.sh
@@ -1,39 +1,88 @@
 #!/bin/sh
 
-export LINE_N=1
-export START=1
-export END=2
+# Compiles Stockfish optimized for x86 and ARM archs if possible.
 
-while read LINE
-do
-  if [ "${LINE}" = 'Supported archs:' ]; then
-    export START=$(expr ${LINE_N} + 2)
-  fi
+set -e
 
-  if [ "${LINE}" = 'Supported compilers:' ]; then
-    export END=$(expr ${LINE_N} - 2)
-  fi
+verbose=
+while getopts v OPTLET; do
+  case "$OPTLET" in
+    v) verbose=1;;
+    \?) exit 2;;
+  esac
+done
 
-  LINE_N=$(expr ${LINE_N} + 1)
-done << EOT
-$(make help)
-EOT
+flags=$(grep '^flags\b' </proc/cpuinfo | head -n 1)
+flags=" ${flags#*:} "
 
-echo 'Looking for compatible architectures'
-echo 'This can take a while...'
+arch=$(uname -m)
 
-while read ARCH
-do
-  make clean
-  echo ${ARCH}
-  #ERROR_L=$(make -j build ARCH=${ARCH} 2>/dev/stdout >/dev/null | wc -l)
-  make -j build ARCH=${ARCH} >/dev/null 2>/dev/null
-  if ./stockfish uci >/dev/null 2>/dev/null; then
-    exit 0
-  fi
-done << EOT
-$(make help | sed -n "${START},${END}p" | cut -f1 -d' ' | cat)
-EOT
+has_flags () {
+  for flag; do
+    case "${flags}" in
+      *" ${flag} "*) :;;
+      *)
+        if [ -n "${verbose}" ]; then
+          echo >&2 "Missing ${flag} for the next level"
+        fi
+        return 1;;
+    esac
+  done
+}
 
-echo 'No compatible architectures were found' 1>2
-exit 1
+determine_level_x86_64 () {
+  level=""
+  has_flags ssse3 || return 1
+  level="ssse3"
+  has_flags popcnt sse4_1 || return 0
+  level="sse41-popcnt"
+  has_flags avx2 || return 0
+  level="avx2"
+  has_flags bmi2 || return 0
+  level="bmi2"
+  has_flags avx512f || return 0
+  level="avx512"
+}
+
+determine_level_x86_32 () {
+  level=""
+  has_flags sse2 || return 1
+  level="sse2"
+  has_flags popcnt sse4_1 || return 0
+  level="sse41-popcnt"
+}
+
+case "$arch" in
+  "x86_64")
+    if determine_level_x86_64; then
+      arch="x86-64-${level}"
+    else
+      arch="x86-64"
+    fi
+    :;;
+  "x86_32")
+    if determine_level_x86_32; then
+      arch="x86-32-${level}"
+    else
+      arch="x86-32"
+    fi
+    :;;
+  "aarch64")
+    arch="armv8"
+    :;;
+  "aarch32")
+    arch="armv7"
+    :;;
+  *)
+    arch="_"
+    :;;
+esac
+
+make clean
+if [ ${arch} = "_" ]; then
+  echo "Building..."
+  make -j build >/dev/null 2>/dev/null
+else
+  echo "Optimizing for ${arch}..."
+  make -j profile-build ARCH=${arch} >/dev/null 2>/dev/null
+fi
