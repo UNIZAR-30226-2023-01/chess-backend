12e3cfc17e54dc928a3abed85d5812dfb93b1d37
hector <hectortoraltrabajos@gmail.com>
2023-04-30 11:58:18 +0200

    recover de lo perdido

--- api_server/controllers/games.ts
+++ api_server/controllers/games.ts
@@ -3,6 +3,7 @@ import { setStatus } from '@lib/status'
 import { GameModel } from '@models/game'
 import { parseGame } from '@lib/parsers'
 import { TournamentModel } from '@models/tournament'
+// import sgMail from '@sendgrid/mail'
 
 export const getAll = (req: Request, res: Response): void => {
   const { meta, data } = res.locals
@@ -41,60 +42,52 @@ export const getOne = (req: Request, res: Response): void => {
 }
 
 export const notify = (req: Request, res: Response): void => {
-  const ahora = new Date()
-  const dentroDe10Minutos = new Date(ahora.getTime() + 10 * 60000)
-
-  TournamentModel.aggregate([{
-    $lookup: {
-      from: 'Tournament',
-      localField: 'matches',
-      foreignField: '_id',
-      as: 'tournament'
-    }
-  }, {
-    $match: {
-      'tournament.matches.startTime': {
-        $gte: ahora,
-        $lt: dentroDe10Minutos
-      }
-    }
-  }, {
-    $unwind: '$tournament.matches'
-  }, {
-    $match: {
-      'tournament.matches.startTime': {
-        $gte: ahora,
-        $lt: dentroDe10Minutos
-      }
-    }
-  }, {
-    $lookup: {
-      from: 'User',
-      localField: 'tournament.matches.participants.email',
-      foreignField: '_id',
-      as: 'user'
-    }
-  }, {
-    $project: {
-      'tournament.matches.startTime': 1,
-      'user.email': 1
-    }
-  }])
-    .then(resultados => {
-      if (!resultados || resultados.length === 0) {
-        return res
-          .status(204)
-          .json({ status: setStatus(req, 204, 'No Content') })
-      }
+  TournamentModel.find()
+    .then((Tournaments: any) => {
+      const now = new Date()
+      const tenMinutesLater = new Date(now.getTime() + 10 * 60 * 1000)
+      const matches = Tournaments
+        .flatMap((tournament: any) => tournament.matches)
+        .filter((match: any) => {
+          const matchStart = new Date(match.startTime)
+          return matchStart >= now && matchStart <= tenMinutesLater
+        })
 
       return res
         .status(200)
         .json({
-          data: resultados,
-          status: setStatus(req, 200, 'Succesfull')
+          data: matches,
+          status: setStatus(req, 200, 'Successful')
         })
+
+      // const msg = {
+      //   to: email,
+      //   from: 'hi@gracehopper.xyz',
+      //   templateId: 'd-47913d1561644ab4aa8e7e3dc053f8a1',
+      //   dynamicTemplateData: {
+      //     subject: 'Tu rival espera'
+      //   }
+      // }
+
+      // sgMail.setApiKey(String(process.env.SENDGRID_API_KEY))
+      // sgMail.send(msg)
+      //   .then(() => {
+      //     return res
+      //       .status(200)
+      //       .json({
+      //         data: [],
+      //         status: setStatus(req, 0, 'Successful')
+      //       })
+      //   })
+      //   .catch(() => {
+      //     return res
+      //       .status(500)
+      //       .json({
+      //         status: setStatus(req, 500, 'Internal Server Error')
+      //       })
+      //   })
     }).catch(() => {
-      return res
+      res
         .status(500)
         .json({ status: setStatus(req, 500, 'Internal Server Error') })
     })
